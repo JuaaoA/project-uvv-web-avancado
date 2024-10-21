e75e40aec13e9337b95ab2497ca51e70b3dd77e6
JuaaoA <joaoaugusto-santana@hotmail.com>
2024-10-21 02:08:59 -0300

    finalizando trabalho part 2

--- frontend/src/app/mensagem/mensagem.component.ts
+++ frontend/src/app/mensagem/mensagem.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { ModeloMensagem } from './mensagem.model';
 import { ServicoMensagens } from './mensagem.service';
+import { NgClass } from '@angular/common';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mensagem',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './mensagem.component.html',
   styleUrl: './mensagem.component.css'
 })
@@ -13,7 +15,31 @@ export class MensagemComponent {
 
   @Input() modelo : ModeloMensagem = new ModeloMensagem();
 
+  public estaEditando : Boolean = false;
+
   constructor(private servico : ServicoMensagens) {}
+
+  onSubmit(form : NgForm)
+  {
+    // Pegar a mensagem que foi editada no formulario
+    const valor = form.value.formMensagemEditada
+
+    // Verificar se o campo não está vazio
+    if (valor == "")
+    {
+      return;
+    }
+
+    // Confirmar edição
+    this.EditSelf(valor);
+
+    // Tirar Campo Mensagem
+    this.ToggleEdit();
+  }
+
+  ToggleEdit() {
+    this.estaEditando = !this.estaEditando;
+  }
 
   ApagarSelf() {
     this.servico.deletarMensagem(this.modelo)
@@ -29,8 +55,8 @@ export class MensagemComponent {
     });
   }
 
-  EditSelf() {
-    this.servico.editarMensagem(this.modelo, "EU QUERO COMER BANANA")
+  EditSelf(mensagemNova : string) {
+    this.servico.editarMensagem(this.modelo, mensagemNova)
     .subscribe({
       next: (dadosSucesso: any) => {
         console.log(dadosSucesso.myMsgSucesso);
